90a0708e6bdf00d9c4e011178bc708e75664f396
Ivan Demidov <Scrum@list.ru>
2017-12-24 15:50:46 +0300

    test: add case for missing attributes

--- test/test-plugin.js
+++ test/test-plugin.js
@@ -48,13 +48,25 @@ test('should add nanoid to iframe links', async t => {
 });
 
 test('should add nanoid to custome attribute', async t => {
-    const input = '<img data-src="lorem.png" alt="attribute not removed"/>';
-    const html = (await processing(input, {tags: ['iframe'], attributes: ['data-src']})).html;
+    const input = '<img data-src="lorem.png" alt="attribute not removed" />';
+    const html = (await processing(input, {attributes: ['data-src']})).html;
 
     const id = queryString.parse(parser(html)[0].attrs['data-src'].split('?')[1]).v;
     const alt = parser(html)[0].attrs.alt;
     t.truthy(id);
     t.truthy(alt);
     t.is(alt, 'attribute not removed');
+    t.is(id.length, 21);
+});
+
+test('should not remove other attributes', async t => {
+    const input = '<link rel="stylesheet" href="style.css">';
+    const html = (await processing(input)).html;
+
+    const id = queryString.parse(parser(html)[0].attrs['href'].split('?')[1]).v;
+    const rel = parser(html)[0].attrs.rel;
+    t.truthy(id);
+    t.truthy(rel);
+    t.is(rel, 'stylesheet');
     t.is(id.length, 21);
 });
